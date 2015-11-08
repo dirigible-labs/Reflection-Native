9301e0af36f940a022d4ea0181ff44d5c465fbb2
Charles Covey-Brandt <chazcb@gmail.com>
2015-11-08 09:17:50 -0800

    Initial commit w/ a bit of extras for eslint and index.ios.js

---
+++ index.ios.js
@@ -0,0 +1,59 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+
+var React = require('react-native');
+var {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+} = React;
+
+var Reflection = React.createClass({
+  render: function() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Reflection
+        </Text>
+        <Text style={styles.instructions}>
+          Check a box
+        </Text>
+        <Text style={styles.instructions}>
+          Box 1
+          Box 2
+          Box 3
+          Box 4
+        </Text>
+        <Text style={styles.instructions}>
+          Press Cmd+R to reload,{'\n'}
+          Cmd+D or shake for dev menu
+        </Text>
+      </View>
+    );
+  }
+});
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('Reflection', () => Reflection);
