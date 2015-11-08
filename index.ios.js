438b320cded6c2ac053e5e2fc3c1047c65a554b8
Charles Covey-Brandt <chazcb@gmail.com>
2015-11-08 10:37:35 -0800

    Updates to react-native 0.14 including some fixes for Xcode issue finding local version of nvm.

--- index.ios.js
+++ index.ios.js
@@ -4,56 +4,56 @@
  */
 'use strict';
 
-var React = require('react-native');
-var {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
+let React = require('react-native');
+let {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
 } = React;
 
-var Reflection = React.createClass({
-  render: function() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Reflection
-        </Text>
-        <Text style={styles.instructions}>
-          Check a box
-        </Text>
-        <Text style={styles.instructions}>
-          Box 1
-          Box 2
-          Box 3
-          Box 4
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
+let styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
 });
 
-var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+const Reflection = React.createClass({
+    render: function() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.welcome}>
+                    Reflection
+                </Text>
+                <Text style={styles.instructions}>
+                    Check a box
+                </Text>
+                <Text style={styles.instructions}>
+                    Box 1
+                    Box 2
+                    Box 3
+                    Box 4
+                </Text>
+                <Text style={styles.instructions}>
+                    Press Cmd+R to reload,{'\n'}
+                    Cmd+D or shake for dev menu
+                </Text>
+            </View>
+        );
+    }
 });
 
 AppRegistry.registerComponent('Reflection', () => Reflection);
