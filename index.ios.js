2c7daccf32f57ef8cd6def495fc487b0da83a7e5
Charles Covey-Brandt <chazcb@gmail.com>
2016-01-06 07:54:10 -0800

    upgrade react native, use node v5.3

--- index.ios.js
+++ index.ios.js
@@ -4,53 +4,101 @@
  */
 'use strict';
 
-let React = require('react-native');
-let {
+const AwesomeButton = require('react-native-awesome-button');
+const React = require('react-native');
+const {
     AppRegistry,
+    AsyncStorage,
     StyleSheet,
     Text,
+    TextInput,
     View,
 } = React;
 
-let styles = StyleSheet.create({
+
+const STORAGE_KEY = '@Reflection:primary_storage';
+
+const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
-        alignItems: 'center',
+        flexDirection: 'column',
         backgroundColor: '#F5FCFF',
+    },
+    row: {
+        flex: 1,
+        backgroundColor: '#efefef',
     },
     welcome: {
         fontSize: 20,
         textAlign: 'center',
         margin: 10,
     },
-    instructions: {
-        textAlign: 'center',
-        color: '#333333',
-        marginBottom: 5,
+    loginButtonBackground: {
+        flex: 1,
+        height: 40,
+        borderRadius: 5,
+        margin: 20,
     },
+    loginButtonLabel: {
+        color: 'white'
+    }
 });
 
 const Reflection = React.createClass({
+
+    getInitialState: function () {
+        let state = AsyncStorage.getItem(STORAGE_KEY)
+        return {
+            username: 'hello',
+            buttonState: 'idle',
+        }
+    },
+
+    logIn: function () {
+        this.setState({ buttonState: 'busy' })
+        setTimeout(() => {
+          this.setState({ buttonState: 'success' })
+        }, 2000);
+    },
+
     render: function() {
         return (
             <View style={styles.container}>
-                <Text style={styles.welcome}>
-                    Reflection
-                </Text>
-                <Text style={styles.instructions}>
-                    Check a box
-                </Text>
-                <Text style={styles.instructions}>
-                    Box 1
-                    Box 2
-                    Box 3
-                    Box 4
-                </Text>
-                <Text style={styles.instructions}>
-                    Press Cmd+R to reload,{'\n'}
-                    Cmd+D or shake for dev menu
-                </Text>
+                <View style={styles.quarterHeight}>
+                    <TextInput
+                        style={styles.welcome}
+
+                        keyboardType="email-address"
+                        onChangeText={(username) => this.setState({username})}
+                        value={this.state.username}
+                    />
+
+                </View>
+                <View style={styles.quarterHeight}>
+                    <AwesomeButton
+                        backgroundStyle={styles.loginButtonBackground}
+                        labelStyle={styles.loginButtonLabel}
+                        transitionDuration={200}
+                        states={{
+                          idle: {
+                            text: 'Log In',
+                            onPress: this.logIn,
+                            backgroundColor: '#1155DD',
+                          },
+                          busy: {
+                            // text: 'Logging In',
+                            backgroundColor: '#002299',
+                            spinner: true,
+                          },
+                          success: {
+                            text: 'Logged In',
+                            backgroundColor: '#339944'
+                          }
+                        }}
+                        buttonState={this.state.buttonState}
+                    />
+                </View>
             </View>
         );
     }
